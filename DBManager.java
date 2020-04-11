cb82050322466180d592a346753c7dd540f4c85c
dshittabey <47832611+dshittabey@users.noreply.github.com>
2020-04-11 13:26:29 -0600

    Update DBManager.java

--- DBManager.java
+++ DBManager.java
@@ -5,15 +5,27 @@ import java.util.ArrayList;
 public class DBManager implements Constants {
 	
 	ArrayList <Course> courseList;
+	ArrayList <Student> studentList;
 
 	public DBManager () {
 		courseList = new ArrayList<Course>();
+		studentList = new ArrayList<Student>();
 	}
 
 	public ArrayList readFromDataBase() {
 		courseList.add(new Course ("ENGG", 233));
 		courseList.add(new Course ("ENSF", 409));
 		courseList.add(new Course ("PHYS", 259));
+		courseList.add(new Course ("ENGG", 209));
+		courseList.add(new Course ("ENGG", 213));
+		courseList.add(new Course ("CHEM", 209));
+		courseList.add(new Course ("MATH", 211));
+		courseList.add(new Course ("MATH", 277));
+		courseList.add(new Course ("MATH", 275));
+		courseList.add(new Course ("ENGG", 201));
+		courseList.add(new Course ("ENGG", 200));
+		courseList.add(new Course ("ENGG", 225));
+		courseList.add(new Course ("ENGG", 202));
 		
 		for (Course i: courseList) {
 			for (int j = 1; j <= NUMSECTIONS; j++) {
