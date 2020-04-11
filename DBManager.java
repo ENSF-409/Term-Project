fbcacffa33546ffec407f08154e13d1f39523edc
dshittabey <47832611+dshittabey@users.noreply.github.com>
2020-04-11 14:53:52 -0600

    Update DBManager.java

--- DBManager.java
+++ DBManager.java
@@ -26,6 +26,14 @@ public class DBManager implements Constants {
 		courseList.add(new Course ("ENGG", 200));
 		courseList.add(new Course ("ENGG", 225));
 		courseList.add(new Course ("ENGG", 202));
+		studentList.add("John", 123);
+		studentList.add("Mary", 343);
+		studentList.add("Niyousha", 345);
+		studentList.add("Dunsin", 987);
+		studentList.add("Francis", 453);
+		studentList.add("Joy", 278);
+		studentList.add("Prince", 145);
+		studentList.add("Helen", 943);
 		
 		for (Course i: courseList) {
 			for (int j = 1; j <= NUMSECTIONS; j++) {
