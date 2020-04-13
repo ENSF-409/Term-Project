906771ebb4900baa06bb68f58af0feebdd2b7e95
dshittabey <47832611+dshittabey@users.noreply.github.com>
2020-04-13 14:40:07 -0600

    Delete Constants.java

--- ServerEdit/serverModel/Constants.java
+++
@@ -1,8 +0,0 @@
-package serverModel;
-
-public interface Constants {
-	static final int MAXCOURSES = 6;
-	static final int MINREG = 8;
-	static final int NUMSECTIONS = 4;
-	static final int CAPACITY = 100;
-}
