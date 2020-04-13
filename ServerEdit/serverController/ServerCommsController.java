71be95e84dc1ea60107c8c61598182635e691b60
dshittabey <47832611+dshittabey@users.noreply.github.com>
2020-04-13 12:44:40 -0600

    Add files via upload

---
+++ ServerEdit/serverController/ServerCommsController.java
@@ -0,0 +1,34 @@
+package serverController;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerCommsController{
+	
+	private ServerSocket serverSocket; //socket to connect server with client
+	private ExecutorService threadPool; //create multiple threads for different users to use services at the same time
+	
+	ServerCommsController(int port){
+		try{
+			serverSocket = new ServerSocket(port); //port should be same as client [9090]
+			threadPool = Executors.newCachedThreadPool(); //create threads as needed
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void communicateWithClient() throws IOException{
+		try {
+			while(true) {
+				BackEnd theBackEnd = new BackEnd(serverSocket.accept()); //Creates new BackEnd instance for every new ComController
+				threadPool.execute(theBackEnd);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			threadPool.shutdown();
+		}
+	}
+}
